a9183422bcbc50e1997f1fb628bbcdf9b3845b07
daluo <gcsdaluo@126.com>
2023-12-19 09:34:03 +0800

    完善 map

--- slice/map.go
+++ slice/map.go
@@ -1,8 +1,28 @@
 package slice
 
+// ReduceMap 执行切片归约操作
+// 即重新对字符串进行 连接-长度累加-统计大写字母-整数累乘等操作
+func ReduceMap[Src any, Dst any](src []Src, reducer func(Dst, Src) Dst, initialValue Dst) Dst {
+	result := initialValue
+	for _, elem := range src {
+		result = reducer(result, elem)
+	}
+	return result
+}
+
 // FilterMap 执行过滤并且转化
 // 如果 m 的第二个返回值是 false，那么我们会忽略第一个返回值
 // 即便第二个返回值是 false，后续的元素依旧会被遍历
+func FilterMap[Src any, Dst any](src []Src, mapper func(idx int, src Src) (Dst, bool)) []Dst {
+	res := make([]Dst, 0, len(src))
+	for index, elem := range src {
+		dst, ok := mapper(index, elem)
+		if ok {
+			res = append(res, dst)
+		}
+	}
+	return res
+}
 
 // ToMap  功能函数--构造泛型map 将src切片slice转化为一个Map
 // map 本身就具备去重功能
